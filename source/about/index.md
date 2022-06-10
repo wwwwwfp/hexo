da82824f13c374bc9a57335c527b633265dc4a78
Chase <wfp_chase@163.com>
2022-06-11 01:07:38 +0800

    about 页

---
+++ source/about/index.md
@@ -0,0 +1,6 @@
+layout: about
+---
+
+---
+
+<center>会走之前得先学会爬</center>
--- themes/fluid/source/js/diy/cursor.js
+++ themes/fluid/source/js/diy/cursor.js
@@ -7,7 +7,7 @@
 
 (function fairyDustCursor() {
 
-  var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+  var possibleColors = ["#ffffff", "#748bff", "#000000"]
   var width = window.innerWidth;
   var height = window.innerHeight;
   var cursor = { x: width / 2, y: width / 2 };
